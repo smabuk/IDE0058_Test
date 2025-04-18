ec6cb71a886065634a321c2928cc91eb2e59a839
smabuk <2011834+smabuk@users.noreply.github.com>
2025-04-18 09:37:39 +0100

    Add project files.

---
+++ UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿namespace IDE0058_Test;
+
+public class UnitTest1
+{
+	private class Customer
+	{
+		public string? Name { get; set; }
+		public int Age { get; set; }
+	}
+
+	[Fact]
+	public void Assignment_ShouldSucceed()
+	{
+		Customer? customer = new();
+		customer?.Name = "Default Name";
+	}
+
+	[Fact]
+	public void Assignment_ShouldFail()
+	{
+		Customer? customer = null;
+		customer?.Name = "Default Name";
+	}
+}
